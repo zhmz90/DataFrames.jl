0317b4f3513b39c48a5a3e4908a34b576463c5c9
Douglas Bates <dmbates@gmail.com>
2014-06-23 16:44:34 -0500

    Preliminary work on simplifying and extending ModelFrame and ModelMatrix

--- src/statsmodels/formula.jl
+++ src/statsmodels/formula.jl
@@ -2,8 +2,7 @@
 # Original by Harlan D. Harris.  Later modifications by John Myles White
 # and Douglas M. Bates.
 
-## Formulas are written as expressions and parsed by the Julia parser.
-## For example :(y ~ a + b + log(c))
+## Formulas are written with a ~ and parsed as a call to @~
 ## In Julia the & operator is used for an interaction.  What would be written
 ## in R as y ~ a + b + a:b is written :(y ~ a + b + a&b) in Julia.
 ## The equivalent R expression, y ~ a*b, is the same in Julia
@@ -26,11 +25,8 @@ end
 
 type Terms
     terms::Vector
-    eterms::Vector        # evaluation terms
-    factors::Matrix{Int8} # maps terms to evaluation terms
-    order::Vector{Int}    # orders of rhs terms
-    response::Bool        # indicator of a response, which is eterms[1] if present
-    intercept::Bool       # is there an intercept column in the model matrix?
+    eterms::Set                       # evaluation terms
+    lhs::Union(Symbol, Expr, Nothing)
 end
 
 type ModelFrame
@@ -47,107 +43,100 @@ end
 Base.size(mm::ModelMatrix) = size(mm.m)
 Base.size(mm::ModelMatrix, dim...) = size(mm.m, dim...)
 
-function Base.show(io::IO, f::Formula)
+Base.show(io::IO, f::Formula) =
     print(io,
           string("Formula: ",
                  f.lhs == nothing ? "" : f.lhs, " ~ ", f.rhs))
-end
+
+## Check if an expression is a call to a particular symbol or is in a set or vector of symbols
+iscall(ex::Expr,s::Symbol) = ex.head == :call && ex.args[1] == s
+iscall(ex::Expr,s::Set)    = ex.head == :call && in(ex.args[1],s)
+iscall(ex::Expr,s::Vector) = ex.head == :call && in(ex.args[1],s)
+iscall(ex,      s)         = false
 
 ## Return, as a vector of symbols, the names of all the variables in
 ## an expression or a formula
 function allvars(ex::Expr)
-    if ex.head != :call error("Non-call expression encountered") end
+    ex.head == :call || error("Non-call expression encountered")
     [[allvars(a) for a in ex.args[2:end]]...]
 end
 allvars(f::Formula) = unique(vcat(allvars(f.rhs), allvars(f.lhs)))
-allvars(sym::Symbol) = [sym]
+allvars(s::Symbol) = [s]
 allvars(v::Any) = Array(Symbol, 0)
 
-# special operators in formulas
-const specials = Set([:+, :-, :*, :/, :&, :|, :^])
+immutable Subsets{T}  # an iterator giving the subsets of elements of a vector
+    nsub::Uint
+    v::AbstractVector{T}
+end
+function Subsets(v::AbstractVector)
+    lv = length(v)
+    lv ≥ 8 * sizeof(lv) && error("length(v) is too large for Subsets")
+    Subsets(uint(1)<<lv,v)
+end
+Base.length(ss::Subsets) = ss.nsub
 
-function dospecials(ex::Expr)
-    if ex.head != :call error("Non-call expression encountered") end
-    a1 = ex.args[1]
-    if !(a1 in specials) return ex end
-    excp = copy(ex)
-    excp.args = vcat(a1,map(dospecials, ex.args[2:end]))
-    if a1 != :* return excp end
-    aa = excp.args
-    a2 = aa[2]
-    a3 = aa[3]
-    if length(aa) > 3
-        excp.args = vcat(a1, aa[3:end])
-        a3 = dospecials(excp)
+Base.start(ss::Subsets) = uint(0)
+function Base.next{T}(ss::Subsets{T},state) 
+    vv = T[]
+    j = state
+    for el in ss.v
+        bool(j & 1) && push!(vv,el)
+        j >>>= 1
     end
-    :($a2 + $a3 + $a2 & $a3)
+    (vv,state+one(Uint))
 end
-dospecials(a::Any) = a
+Base.done(ss::Subsets,state) = state ≥ ss.nsub
 
-const associative = Set([:+,:*,:&])       # associative special operators
-
-## If the expression is a call to the function s return its arguments
-## Otherwise return the expression
-function ex_or_args(ex::Expr,s::Symbol)
-    if ex.head != :call error("Non-call expression encountered") end
-    excp = copy(ex)
+## Expand calls to * within any of the arguments of the special operators
+function xpndmlt(ex)
+    iscall(ex,[:+, :-, :*, :/, :&, :|, :^]) || return ex
     a1 = ex.args[1]
-    a2 = map(condense, ex.args[2:end])
-    if a1 == s return a2 end
-    excp.args = vcat(a1, a2)
-    excp
+    ex = Expr(:call,a1,[xpndmlt(t) for t in ex.args[2:end]]...)  # recursive application
+    a1 == :* || return ex
+    sumands = {}
+    for s in Subsets(ex.args[2:end])
+        len = length(s)
+        len == 1 && push!(sumands,s[1])
+        len > 1 && push!(sumands,Expr(:call,:&,s...))
+    end
+    Expr(:call,:+,sumands...)
 end
-ex_or_args(a,s::Symbol) = a
 
+cargs(aa,s::Symbol) = vcat({iscall(a,s) ? cargs(a.args[2:end],s) : {a} for a in aa}...)
 ## Condense calls like :(+(a,+(b,c))) to :(+(a,b,c))
-## Also need to work out how to distribute & over +
-function condense(ex::Expr)
-    if ex.head != :call error("Non-call expression encountered") end
-    a1 = ex.args[1]
-    if !(a1 in associative) return ex end
-    excp = copy(ex)
-    excp.args = vcat(a1, map(x->ex_or_args(x,a1), ex.args[2:end])...)
-    excp
+function condense(ex,s::Symbol)
+    iscall(ex,s) || return ex
+    Expr(:call,s,cargs(ex.args[2:end],s)...)
 end
-condense(a::Any) = a
 
-getterms(ex::Expr) = (ex.head == :call && ex.args[1] == :+) ? ex.args[2:end] : ex
-getterms(a::Any) = a
+getterms(ex) = iscall(ex,:+) ? ex.args[2:end] : [ex]
 
-ord(ex::Expr) = (ex.head == :call && ex.args[1] == :&) ? length(ex.args)-1 : 1
-ord(a::Any) = 1
-
-const nonevaluation = Set([:&,:|])        # operators constructed from other evaluations
+## order of an interaction
+ord(ex::Expr) = iscall(ex,:&) ? length(ex.args)-1 : (iscall(ex,:|) ? typemax(1) : 1)
+ord(s::Symbol) = 1
+ord(x::Number) = 0
 ## evaluation terms - the (filtered) arguments for :& and :|, otherwise the term itself
-function evt(ex::Expr)
-    if ex.head != :call error("Non-call expression encountered") end
-    if !(ex.args[1] in nonevaluation) return ex end
-    filter(x->!isa(x,Number), vcat(map(getterms, ex.args[2:end])...))
+function evt(ex::Expr,s::Set)
+    iscall(ex,[:&,:|,:+,:/]) || return push!(s,ex)
+    for a in ex.args[2:end]
+        evt(a,s)
+    end
+    s
 end
-evt(a) = {a}
+evt(x::Number,S::Set) = push!(S,x)
+evt(s::Symbol,S::Set) = push!(S,s)
+
+ordterms(rhs) = rhs[sortperm([ord(t) for t in rhs])]
 
 function Terms(f::Formula)
-    rhs = condense(dospecials(f.rhs))
-    tt = getterms(rhs)
-    if !isa(tt,AbstractArray) tt = [tt] end
-    tt = tt[!(tt .== 1)]             # drop any explicit 1's
-    noint = (tt .== 0) | (tt .== -1) # should also handle :(-(expr,1))
-    tt = tt[!noint]
-    oo = int(map(ord, tt))           # orders of interaction terms
-    if !issorted(oo)                 # sort terms by increasing order
-        pp = sortperm(oo)
-        tt = tt[pp]
-        oo = oo[pp]
-    end
-    etrms = map(evt, tt)
-    haslhs = f.lhs != nothing
-    if haslhs
-        unshift!(etrms, {f.lhs})
-        unshift!(oo, 1)
-    end
-    ev = unique(vcat(etrms...))
-    facs = int8(hcat(map(x->(s=Set(x);map(t->int8(t in s), ev)),etrms)...))
-    Terms(tt, ev, facs, oo, haslhs, !any(noint))
+    rhs = condense(xpndmlt(f.rhs),:+)
+    eterms = evt(rhs,Set())             # the set of evaluation terms
+    noint = in(0,eterms)                # at present just use 0 + for suppressing intercept
+    forceint = in(1,eterms)
+    noint && forceint && error("Contradictory indicators 0 + and 1 + in formula")
+    noint || push!(eterms,1)
+    terms = ordterms(getterms(rhs))
+    Terms(ordterms(getterms(rhs)), eterms, f.lhs)
 end
 
 ## Default NA handler.  Others can be added as keyword arguments
@@ -160,11 +149,11 @@ end
 function dropUnusedLevels!(da::PooledDataArray)
     rr = da.refs
     uu = unique(rr)
-    if length(uu) == length(da.pool) return da end
+    length(uu) == length(da.pool) && return da
     T = eltype(rr)
     su = sort!(uu)
     dict = Dict(su, one(T):convert(T,length(uu)))
-    da.refs = map(x->dict[x], rr)
+    da.refs = [dict[x] for x in rr]
     da.pool = da.pool[uu]
     da
 end
@@ -204,20 +193,32 @@ function isfe(ex::Expr)                 # true for fixed-effects terms
 end
 isfe(a) = true
 
-## Expand the columns in an interaction term
-function expandcols(trm::Vector)
-    if length(trm) == 1 return float64(trm[1]) end
-    if length(trm) == 2
-        a = float64(trm[1])
-        b = float64(trm[2])
-        nca = size(a,2)
-        ncb = size(b,2)
-        return hcat([a[:,i].*b[:,j] for i in 1:nca, j in 1:ncb]...)
+## Utility to determine all combinations of indices from vector sizes
+## returns a matrix of size length(v)×prod(v).  Sort of like expand.grid in R.
+function allcomb(v::Vector)
+    inds = {}
+    tot = prod(v)
+    each = 1
+    for el in v
+        push!(inds, rep([1:el], div(tot,el*each), each)')
+        each *= el
     end
-    error("code for 3rd and higher order interactions not yet written")
+    vcat(inds...)
 end
 
-nc(trm::Vector) = *([size(x,2) for x in trm]...)
+## Expand the columns in an interaction term
+## The argument is a vector of matrices
+function expandcols(trms::Vector)
+    trms = [float64(t) for t in trms]
+    (l = length(trms)) == 1 && return trms[1]
+    l1 = size(trms[1],1)
+    all([size(t,1) == l1 for t in trms[2:end]]) ||
+        error("size(t,1) must match for all t in trms")
+    inds = allcomb([size(t,2) for t in trms])
+    hcat([reduce(.*,{a[:,j] for (a,j) in zip(trms,inds[:,jj])}) for jj in 1:size(inds,2)]...)
+end
+
+nc(trms::Vector) = prod([size(t,2) for t in trms])
 
 function ModelMatrix(mf::ModelFrame)
     trms = mf.terms
