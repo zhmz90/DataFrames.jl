a1f995e1d9a1b1c4a83a0320a06b8bb0c2c30073
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2015-03-22 16:24:07 -0700

    explicit tests for formula parsing into Terms
    These were all tested implicitly before by checking the resulting
    model matrices they produce when combined with data, but I think
    explicit tests are better to catch formula parsing errors early

--- test/formula.jl
+++ test/formula.jl
@@ -9,7 +9,92 @@ module TestFormula
     # - implement ^2 for datavector's
     # - support more transformations with I()?
 
-    #test_group("Basic tests")
+    ## Formula parsing
+    import DataFrames.Terms
+
+    ## totally empty
+    t = Terms(Formula(nothing, 0))
+    @test t.response == false
+    @test t.intercept == false
+    @test t.terms == []
+    @test t.eterms == []
+
+    ## empty RHS
+    t = Terms(y ~ 0)
+    @test t.intercept == false
+    @test t.terms == []
+    @test t.eterms == [:y]
+    t = Terms(y ~ -1)
+    @test t.intercept == false
+    @test t.terms == []
+
+    ## intercept-only
+    t = Terms(y ~ 1)
+    @test t.response == true
+    @test t.intercept == true
+    @test t.terms == []
+    @test t.eterms == [:y]
+    
+    ## terms add
+    t = Terms(y ~ 1 + x1 + x2)
+    @test t.intercept == true
+    @test t.terms == [:x1, :x2]
+    @test t.eterms == [:y, :x1, :x2]
+
+    ## implicit intercept behavior:
+    t = Terms(y ~ x1 + x2)
+    @test t.intercept == true
+    @test t.terms == [:x1, :x2]
+    @test t.eterms == [:y, :x1, :x2]
+
+    ## no intercept
+    t = Terms(y ~ 0 + x1 + x2)
+    @test t.intercept == false
+    @test t.terms == [:x1, :x2]
+    
+    t = Terms(y ~ -1 + x1 + x2)
+    @test t.intercept == false
+    @test t.terms == [:x1, :x2]
+
+    t = Terms(y ~ x1 & x2)
+    @test t.terms == [:(x1 & x2)]
+    @test t.eterms == [:y, :x1, :x2]
+
+    ## `*` expansion
+    t = Terms(y ~ x1 * x2)
+    @test t.terms == [:x1, :x2, :(x1 & x2)]
+    @test t.eterms == [:y, :x1, :x2]
+
+    ## associative rule:
+    ## +
+    t = Terms(y ~ x1 + x2 + x3)
+    @test t.terms == [:x1, :x2, :x3]
+
+    ## &
+    t = Terms(y ~ x1 & x2 & x3)
+    @test t.terms == [:((&)(x1, x2, x3))]
+    @test t.eterms == [:y, :x1, :x2, :x3]
+
+    ## distributive property of + and &
+    t = Terms(y ~ x1 & (x2 + x3))
+    @test t.terms == [:(x1&x2), :(x1&x3)]
+
+    ## FAILS: ordering of expanded interaction terms is wrong
+    ## (only has an observable effect when both terms are categorical and
+    ## produce multiple model matrix columns that are multiplied together...)
+    ## 
+    ## t = Terms(y ~ (x2 + x3) & x1)
+    ## @test t.terms == [:(x2&x1), :(x3&x1)]
+
+    ## three-way *
+    t = Terms(y ~ x1 * x2 * x3)
+    @test t.terms == [:x1, :x2, :x3,
+                      :(x1&x2), :(x1&x3), :(x2&x3),
+                      :((&)(x1, x2, x3))]
+    @test t.eterms == [:y, :x1, :x2, :x3]
+
+
+    ## Tests for constructing ModelFrame and ModelMatrix
 
     d = DataFrame()
     d[:y] = [1:4;]
